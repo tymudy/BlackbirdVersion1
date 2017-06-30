0f7c932ed73c2776c795f33af330d6cc0f611e5a
tymudy <tymudy@yahoo.com>
2017-06-30 18:26:09 +0300

    newVersionbtnController

--- Button Controller/src/app/gallery/gallery-spec/spec-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector:'gallery-spec',
-  templateUrl:'./spec-list.component.html',
-  styleUrls:['./spec-list.component.css']
-})
-export class GallerySpecComponent {
-}
