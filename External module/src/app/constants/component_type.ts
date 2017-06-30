212ed9de4106edd5f5d9f069cb5bc668c51c0825
tymudy <tymudy@yahoo.com>
2017-06-30 18:29:50 +0300

    newVersion

--- External module/src/app/constants/component_type.ts
+++
@@ -1,6 +0,0 @@
-import {ButtonComponent} from "../button/button.component";
-import {SliderComponent} from "../slider/slider.component";
-
-export const types = {'button': ButtonComponent,
-  'slider': SliderComponent
-};
